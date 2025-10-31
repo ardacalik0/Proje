de9dee50d4cd856aa01df2e1d531d2081025914a
[ardacalik0] <[ardacalik0@gmail.com]>
2025-10-31 05:16:12 +0300

    Initial commit : Java Hesap Makinesi

---
+++ src/hesapmakinesi/HesapMakinesi.java
@@ -0,0 +1,59 @@
+package hesapmakinesi;
+
+import java.util.Scanner;
+
+public class HesapMakinesi {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        double sayi1, sayi2, sonuc = 0;
+        char operator;
+
+       
+        System.out.print("İlk sayıyı giriniz: ");
+        sayi1 = input.nextDouble();
+
+        
+        System.out.print("İşlem operatörünü giriniz (+, -, *, /): ");
+        operator = input.next().charAt(0);
+
+       
+       
+        System.out.print("İkinci sayıyı giriniz: ");
+        sayi2 = input.nextDouble();
+
+    
+        switch(operator) {
+            case '+':
+                sonuc = sayi1 + sayi2;
+                break;
+            case '-':
+                sonuc = sayi1 - sayi2;
+                break;
+            case '*':
+                sonuc = sayi1 * sayi2;
+                break;
+            case '/':
+                if (sayi2 != 0) {
+                    sonuc = sayi1 / sayi2;
+                } else {
+                    System.out.println("Hata! Sıfıra bölme yapılamaz.");
+                    return; 
+                }
+                break;
+            default:
+                System.out.println("Hata! Geçersiz operatör.");
+                return; 
+        }
+
+        // Sonucu ekrana yazdır
+        System.out.println("\nSonuç: " + sayi1 + " " + operator + " " + sayi2 + " = " + sonuc);
+        
+        input.close(); 
+    }
+
+
+	}
+	
+
